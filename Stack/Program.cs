c34025bfa25d5449590e9e41283473ca1c73b3de
Qu <Quan0027@elec.techcollege.dk>
2019-12-18 15:00:09 +0100

    building a stack

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -10,6 +10,17 @@ namespace Stack
     {
         static void Main(string[] args)
         {
+            var stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop().ToString());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Stack/Stack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Stack
+    {
+        
+        private List<object> _list = new List<object>();
+
+       public void Push(object obj)
+       {
+           if (obj == null)
+           {
+               throw new InvalidOperationException();
+           }
+           else
+           {
+               var _list = obj;
+
+               this._list.Add(obj);
+               
+           }
+
+       }
+
+        public object Pop()
+        {
+            _list.Remove(_list.Count-1);
+            return _list.ToString();
+        }
+
+        //public void Clear()
+        //{
+
+        //}
+     
+    }
+}
